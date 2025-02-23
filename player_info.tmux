b04f68b6318c4a414a6a9738c356c48963bfa6c0
KacKoz <kacpro@onet.com.pl>
2025-02-23 01:25:24 +0100

    Test 3

--- player_info.tmux
+++ player_info.tmux
@@ -16,7 +16,6 @@ set_tmux_option() {
 	local option="$1"
 	local value="$2"
 	tmux set-option -gq "$option" "$value"
-	echo "$option $value"
 }
 
 do_interpolation() {
--- scripts/player_info.sh
+++ scripts/player_info.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# microsec_length=$(playerctl metadata mpris:length)
-# sec_length=${microsec_length::-6}
-# sec_position=$(playerctl position)
-#
-# position=$(date -d@$sec_position -u +%M:%S)
-# length=$(date -d@$sec_length -u +%M:%S)
+microsec_length=$(playerctl metadata mpris:length)
+sec_length=${microsec_length::-6}
+sec_position=$(playerctl position)
 
-# echo "$(playerctl metadata xesam:title) $position/$length"
-echo "ELko Melko"
+position=$(date -d@$sec_position -u +%M:%S)
+length=$(date -d@$sec_length -u +%M:%S)
+
+echo "$(playerctl metadata xesam:title) $position/$length"
+# echo "ELko Melko"
 
